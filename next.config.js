1e498f21d7f4c3e07711a6c4f58c48b8f46ce98c
swpark <parksw8218@gmail.com>
2022-11-07 10:14:57 +0900

    BoilerPlate 추가

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        BASE_URL: process.env.BASE_URL,
+    },
+    webpack(conf) {
+        conf.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        svgoConfig: {
+                            plugins: [
+                                {
+                                    // Enable figma's wrong mask-type attribute work
+                                    removeRasterImages: false,
+                                    removeStyleElement: false,
+                                    removeUnknownsAndDefaults: false,
+                                    // Enable svgr's svg to fill the size
+                                    removeViewBox: false,
+                                },
+                            ],
+                        },
+                    },
+                },
+            ],
+        });
+        // 절대경로
+        conf.resolve.modules.push(__dirname);
+        return conf;
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+};
+
+module.exports = nextConfig;
